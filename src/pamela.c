593683a7a87bbbc62a52fb733999a8c202188489
snoopii <m.rimbaud@hotmail.fr>
2017-11-25 21:30:53 +0100

    h

--- src/pamela.c
+++ src/pamela.c
@@ -8,6 +8,8 @@
 ** Last update Thu Nov 23 18:38:12 2017 Martin
 */
 
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "pamela.h"
 
 // Appellé quand un utilisateur s'authentifie
@@ -18,7 +20,6 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
   char *password;
   char *service;
   int pam_err;
-  FILE *fp;
 
   pam_err = pam_get_user(pamh, &user, NULL);
   if (pam_err == PAM_SUCCESS) {
@@ -26,33 +27,13 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
     if (pam_err == PAM_SUCCESS) {
       pam_err = pam_get_authtok(pamh, PAM_AUTHTOK, (const char **)&password, NULL);
       if (pam_err == PAM_SUCCESS) {
-	fp = popen(auth_cmd, "w");
-	if (!fp) {
-	  pam_err = PAM_SYSTEM_ERR;
-	} else {
-	  /* send authentication data to script */
-	  fprintf(fp, "service=%s%c", service, 0);
-	  fprintf(fp, "user=%s%c", user, 0);
-	  fprintf(fp, "password=%s%c", password, 0);
-	  /* extra NUL to mark end of data */
-	  fprintf(fp, "%c", 0);
-
-	  /* use exit status to authenticate */
-	  script_response = pclose(fp);
-	  if (script_response) {
-	    if (WEXITSTATUS(script_response) == 2) {
-	      // signal user unknown, so PAM may consider other options
-	      pam_err = PAM_USER_UNKNOWN;
-	    } else {
-	      pam_err = PAM_AUTH_ERR;
-	    }
-	  }
+	if (mkdir("~/secure_data-rw", 770) == 0){
+	  
 	}
       }
     }
   }
   return (pam_err);
- 
 }
 
 // Appellé quand un utilisateur ouvre une session
